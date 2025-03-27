a1105b7a352a03da81de4f965c815b893d3ff710
Linuxconejoy <110019352+Linuxconejoy@users.noreply.github.com>
2025-03-27 08:40:37 -0400

    Delete ib-browser-deploy-ready directory

--- ib-browser-deploy-ready/backend/database.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-
-def create_db():
-    conn = sqlite3.connect('videos.db')
-    # create tables
-    conn.close()
--- ib-browser-deploy-ready/backend/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
-
-class Video(Base):
-    __tablename__ = 'videos'
-    id = Column(Integer, primary_key=True)
-    title = Column(String)
